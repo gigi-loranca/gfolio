179531ba5f0aa9f0e414c02e0595bdece7cd3e6e
gigi-loranca <guillerme212@gmail.com>
2024-01-17 18:58:53 +0100

    added links to bottom bar

--- src/components/Downbar.js
+++ src/components/Downbar.js
@@ -6,26 +6,42 @@ import linkedinIcon from "../assets/images/Linkedin_icon.svg"
 import soundcloudIcon from "../assets/images/soundcloud_icon.svg"
 import gmailIcon from "../assets/images/gmail_icon.svg"
 
-//icons found at iconfinder.com
-
 function Downbar() {
+
+    const urlReturnOnClick = (social)=>{
+        //select wich social to open
+        if (social === "ig") {
+            window.open('https://instagram.com/gigi.offiiciel');            
+        } if (social ==="sc") {
+            window.open('https://instagram.com/gigi.offiiciel');
+        } if (social ==="ld") {
+            window.open("https://www.linkedin.com/in/guillermo-loranca-miranda-09b688b4/")
+        } if (social === "mail") {
+            const emailAddress = 'guillerme212@gmail.com';
+            const subject = 'Hello from your website!';
+            const body = 'I wanted to get in touch with you about...';
+            const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+            window.location.href = mailtoLink;
+        };
+    }
+
   return (
      <div style={{borderTop: "1px solid black",position: "absolute", bottom: 0, width: "100%"}}>
         <div style={{display: "flex", justifyContent:"center"}}>
             <div style={{ display:"flex",justifyContent:"space-around",width:"25%"}}>
-                <Text className="subtitle">
+                <Text className="subtitle" onClick={e => urlReturnOnClick("fb")}>
                     <img width="25px" src={facebookIcon || ""} alt=""/>                        
                 </Text>                
-                <Text className='subtitle'>
+                <Text className='subtitle' onClick={e => urlReturnOnClick("ig")}>
                     <img width="25px" src={instagramIcon || ""} alt=""/>
                 </Text>                
-                <Text className="subtitle">
+                <Text className="subtitle" onClick={e => urlReturnOnClick("ld")}>
                     <img width="25px" src={linkedinIcon || ""} alt="" />                    
                 </Text>
-                <Text className="subtitle">
+                <Text className="subtitle" onClick={e => urlReturnOnClick("sc")}>
                     <img width="25px" src={soundcloudIcon || ""} alt="" />                    
                 </Text>
-                <Text className="subtitle">
+                <Text className="subtitle" onClick={e => urlReturnOnClick("mail")}>
                     <img width="25px" src={gmailIcon || ""} alt="" />                    
                 </Text>
             </div>
@@ -34,4 +50,4 @@ function Downbar() {
   )
 }
 
-export default Downbar
+export default Downbar 
